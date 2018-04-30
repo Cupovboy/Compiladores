655681988dfe8c8843169eebd921267d8e4d4f67
ivan martinez escobedo <pedromartinez23@gmail.com>
2018-04-29 19:01:13 -0500

    Tests

--- cuadruplos.py
+++ cuadruplos.py
@@ -6,6 +6,3 @@ class cuadruplo:
         self.var1 = var1
         self.var2 = var2
         self.var3 = var3
-
-    def __str__(self):
-        return str(self.dir) + "\t" + str(self.estatuto) + "\t" + str(self.var1) + "\t" + str(self.var2) + "\t" + str(self.var3)
